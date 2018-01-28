dfc425891c9c71cbb6eea808d8e05d10acb89445
Tim Etchells <tetchel@gmail.com>
2018-01-28 06:32:56 -0500

    Working on ears, PlayerEars still WIP

---
+++ Assets/Scripts/Actors/Ears/NearbyPlayerEars.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// If the player is within a small radius, the enemy hears them
+public class NearbyPlayerEars : EnemyEars {
+
+    protected override float getRadius() {
+        return 3f;
+    }
+
+    protected override void Hear(GameObject obj) {
+        // The enemy has a 'hearing radius'. If the player passes through this circle, the enemy is alerted.
+
+        Vector2? sourceOfNoise = null;
+        Player player = obj.GetComponent<Player>();
+        if (player != null) {
+
+            // Can't hear through walls
+            LayerMask layerMask = -1;
+            int mask = 1 << 2;
+            layerMask.value &= ~mask;
+            mask = 1 << 8;
+            layerMask.value &= ~mask;
+            RaycastHit2D castHit = Physics2D.Raycast(owner.transform.position, obj.transform.position);// Mathf.Infinity, layerMask);
+
+            if (castHit.transform != null && castHit.collider.gameObject.CompareTag("Player")) {
+                // Send the enemy after the player!
+                sourceOfNoise = player.transform.position;
+            }
+
+        }
+
+        if (sourceOfNoise != null) {
+            Debug.Log("SourceOfNoise: " + sourceOfNoise);
+            owner.HearNoise(new Enemy.PlayerLocation((Vector2)sourceOfNoise, 1));
+        }
+    }
+}
---
+++ Assets/Scripts/Actors/Ears/ProjectileEars.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Listens for nearby Bullets
+public class ProjectileEars : EnemyEars {
+
+    protected override float getRadius() {
+        return 1.5f;
+    }
+
+    protected override void Hear(GameObject obj) {
+        // The enemy has a 'hearing radius'. If the player or a projectile passes through this circle, the enemy is alerted.
+
+        Vector2? sourceOfNoise = null;
+        Bullet bullet = obj.GetComponent<Bullet>();
+        if (bullet != null) {
+            // The enemy is able to determine where the bullet was fired from from the bullet's rotation.
+            Vector2 bulletSourceDirection = -bullet.transform.up;
+            Debug.Log("Shot from " + bulletSourceDirection);
+
+            // We'll send the enemy to the location of the first thing the ray hits.
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, bulletSourceDirection);
+            sourceOfNoise = hit.transform.position;
+        }
+
+        if (sourceOfNoise != null) {
+            Debug.Log("SourceOfNoise: " + sourceOfNoise);
+            owner.HearNoise(new Enemy.PlayerLocation((Vector2)sourceOfNoise, 1));
+        }
+    }
+}
