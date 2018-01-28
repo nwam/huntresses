bda924d8c0ccdfda78ffa475dd6ba20af0ca04a6
Tim Etchells <tetchel@gmail.com>
2018-01-27 22:10:11 -0500

    Big ass merge of Enemy and Player, plus sorting of scripts

---
+++ Assets/Scripts/Actors/Corpse.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Corpse : MonoBehaviour {
+
+    [SerializeField]
+    private float bloodCapacity = 10f;
+    [SerializeField]
+    private float drainRate = 1f;
+
+    private IHarvester currentHarvester = null;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        IHarvester newHarvester = collision.gameObject.GetComponent<IHarvester>();
+
+        if (newHarvester != null) {
+            newHarvester.AddHarvestTarget(this);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        IHarvester oldHarvester = collision.gameObject.GetComponent<IHarvester>();
+
+        if (oldHarvester != null) {
+            oldHarvester.RemoveHarvestTarget(this);
+        }
+    }
+
+    public float Harvest(IHarvester harvester) {
+        // Make sure nobody else is harvesting this corpse
+        if (currentHarvester != null && currentHarvester != harvester) {
+            return 0f;
+        }
+
+        // Mark this corpse as being harvested
+        currentHarvester = harvester;
+
+        float drained = drainRate * Time.deltaTime;
+
+        if (bloodCapacity > drained) {
+            bloodCapacity -= drained;
+        }
+        else {
+            drained = bloodCapacity;
+            bloodCapacity = 0;
+        }
+        return drained;
+    }
+
+    public void StopHarvest(IHarvester harvester) {
+        if (harvester == currentHarvester) {
+            currentHarvester = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Actors/Items/BloodPool.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BloodPool : MonoBehaviour {
+    [SerializeField]
+    private float maxDuration = 60;       // In seconds
+
+    [SerializeField]
+    private float drainPerSecond = 0;
+
+    [SerializeField]
+    private Text text;
+
+    [SerializeField]
+    private GameObject needBloodIndicator;
+
+    // amount of blood in the pool - starts full, cannot go below 0
+    private float available;
+
+    private void Start() {
+        available = maxDuration;
+        needBloodIndicator.SetActive(false);
+    }
+
+    private void LateUpdate() {
+        text.text = Mathf.Round(available) + " / " + maxDuration;
+    }
+
+    public bool Withdraw() {
+        float toWithdraw = drainPerSecond * Time.deltaTime;
+        if(toWithdraw <= available) {
+            available -= toWithdraw;
+            return true;
+        }
+        else {
+            // Not enough available
+            StartCoroutine(displayNotEnoughBlood());
+            return false;
+        }        
+    }
+
+    public void Fill(float amount) {
+        available += amount;
+        if(available > maxDuration) {
+            available = maxDuration;
+        }
+    }
+
+    IEnumerator displayNotEnoughBlood() {
+        needBloodIndicator.SetActive(true);
+        yield return new WaitForSeconds(1);
+        needBloodIndicator.SetActive(false);
+    }
+}
